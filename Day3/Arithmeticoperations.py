a075b5b4d5350e0651ee6d5b99ab62fd57dfc48b
sudha-madhuri282 <sudhamadhuri.v@gmail.com>
2025-12-28 17:48:37 +0530

    Day3 files

---
+++ Day3/Arithmeticoperations.py
@@ -0,0 +1,10 @@
+# Performing Arithmetic operations
+x = 10
+y = 3
+print("Addition:", x + y)
+print("Subtraction:", x - y)
+print("Multiplication:", x * y)
+print("Division:",x / y)
+print("Modulus:", x % y)
+print("Floor Division:", x // y)
+print("Exponent:", x ** y)
---
+++ Day3/BuildIndatatypes.py
@@ -0,0 +1,14 @@
+# Creating a list to store student details
+Student = ["Sudha", 25, 45.5, "Sudha Madhuri"]
+print(type(Student))
+# Creating a tuple to store colors
+colors = ("Red", "Green", "Blue")
+print(type(colors))
+# Creating a Set to store unique numbers
+unique_numbers = {1,2,3,4,3,4}
+print(unique_numbers)
+print(type(unique_numbers))
+# Creating a dictionary to store student name
+details={"name": "Sudha","age": "25", "Course": "Python"}
+print(details)
+print(type(details))
